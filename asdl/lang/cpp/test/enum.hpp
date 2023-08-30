764b47fdb1271a7db8c279c58ceb3caed1068a39
Joël Lamotte (Klaim) <joel.lamotte@quantstack.net>
2023-08-30 15:04:19 +0200

    c++ - added then commented tests for enum objects declarations

--- asdl/lang/cpp/test/enum.hpp
+++ asdl/lang/cpp/test/enum.hpp
@@ -22,3 +22,18 @@ enum named {
 enum renamed {
   Some
 };
+
+// Note: the following definitions will not result in exactly the same code generated,
+// they will look like an enum definition followed by an object declaratoin of that enum's type.
+// TODO: support for types definition in object definitions, maybe by checking `TagType::isBeingDefined()`?
+// FIXME: commented examples below because the output is equivalent but not the same.
+
+// enum {
+//   x, y, z,
+// } object;
+
+
+// enum with_name {
+//   u, v, w,
+// } object_with_typed_name;
+
