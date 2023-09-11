a72154cfef32a30707c8ebe1c1a10e943b3afbfc
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2023-09-11 21:31:03 +0200

    c++ syntax - support auto return type and auto parameters
    No support for trailing return type yet!

--- asdl/lang/cpp/test/auto.hpp
+++ asdl/lang/cpp/test/auto.hpp
@@ -3,3 +3,13 @@ const auto cx = 1;
 const auto & cxr = 1;
 decltype(auto) y = 2;
 __auto_type z = 3;
+
+auto foo() {
+  return 1;
+}
+
+auto lambda = [](auto y) { return y; };
+
+auto bar(int x) {
+  return [&x](auto * z) { return *z + x; };
+}
